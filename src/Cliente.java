23f9cc5684cb8992c0e0e84c43e6c05f30b72b89
Davi120906 <163910228+Davi120906@users.noreply.github.com>
2024-06-05 19:15:59 -0300

    Atividade 7

---
+++ src/Cliente.java
@@ -0,0 +1,21 @@
+import java.util.Date;
+public class Cliente {
+
+    String nome;
+    
+    Date data;
+
+    String endereco;
+    public void imprimir(Object obj){
+        System.out.println(obj.toString());
+    }
+    public void comparar(Object obj){
+        if(this.equals(obj) == true){
+            System.out.println("SÃO A MESMA CONTA");
+        }
+        else{
+            System.out.println("NÃO SÃO A MESMA CONTA");
+        }
+    }
+
+}
---
+++ src/PessoasJuridica.java
@@ -0,0 +1,28 @@
+public class PessoasJuridica extends Cliente {
+    String cnpj;
+    int numfuncionarios;
+    String setor;
+    @Override
+    public String toString() {
+        return "Dono: " + super.nome + "\n" +
+               "Endereco: " + super.endereco + "\n" +
+               "CNPJ: " + cnpj + "\n" +
+               "Numero de Funcionarios: " + numfuncionarios + "\n" +
+               "Setor: " + setor + "\n" +
+               "Data de criacao da conta: " + super.data + "\n" +
+               "====================";
+    }
+    @Override 
+    public boolean equals(Object obj){
+        if(obj == null){
+            return false;
+        }
+        PessoasJuridica outraPessoasJuridica= (PessoasJuridica) obj;
+        if(this.cnpj == outraPessoasJuridica.cnpj){
+        return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
